aca7b24eb02a4b4419ed653b4a7176d6a1b4e678
Aditya Arora <adityadvlp@gmail.com>
2019-07-22 11:44:13 +0400

    Create freeze_model.py

---
+++ gist/pytorch/freeze_model.py
@@ -0,0 +1,11 @@
+def freeze(model):
+    for p in model.parameters():
+        p.requires_grad=False
+
+def unfreeze(model):
+    for p in model.parameters():
+        p.requires_grad=True
+
+def is_frozen(model):
+    x = [p.requires_grad for p in model.parameters()]
+    return not all(x)
